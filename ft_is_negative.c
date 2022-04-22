fbdf000639fd3f4d4d1b7c415de49880c81abff2
Israel <israeltemi25@gmail.com>
2022-04-22 01:39:22 +0100

    ft_is_negative

---
+++ ft_is_negative.c
@@ -0,0 +1,19 @@
+
+#include <unistd.h>
+
+void	ft_is_negative(int n)
+{
+	if (n < 0)
+	{
+		write (1, "N", 1);
+	}
+	else if (n >= 0)
+	{
+		write (1, "P", 1);
+	}		
+}
+
+int main(void)
+{
+	ft_is_negative(-2);
+}
